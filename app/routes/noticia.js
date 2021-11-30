232b9a1360d151fd3186b0b866301bfdf209f923
Lucas-Bento-D <lucasbentoskl@outlook.com>
2021-11-30 20:32:13 -0300

    Adicionando modulo para requisiçao de banco de dados

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,6 +1,13 @@
 module.exports = app => {
-    
     app.get('/noticia', (req, res) => {
-        res.render('noticias/noticia');
+        var connection = app.config.dbConnection()
+        var noticiaModel = app.app.models.noticiasModel
+
+        noticiaModel.getNoticia(connection, (error, result) => {
+            res.render('noticias/noticia', {noticia: result});
+        })
+
+        // connection.query('select * from noticias where id_noticia = 1', (error, result) => {
+        // })
     });
 }
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,8 +1,10 @@
 module.exports = app => {
     app.get('/noticias', (req, res) => {
         var connection = app.config.dbConnection()
-        connection.query("select * from noticias", (error, result) =>{
+        var noticiasModel = app.app.models.noticiasModel
+
+        noticiasModel.getNoticias(connection, (error, result) =>{
             res.render('noticias/noticias', {noticias: result});
-        });
+        })
     });
 }
