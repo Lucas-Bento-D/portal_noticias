3180cf797cae63a3f80745b3b2d1cce23afc1dbe
Lucas-Bento-D <lucasbentoskl@outlook.com>
2021-12-02 23:40:59 -0300

    Implementa funçao para inserir um item no banco de dados e redirecionar para a pagina de noticias + explicaçoes

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -9,5 +9,12 @@ module.exports = () => {
         connection.query('select * from noticias where id_noticia = 1', callback)
     }
 
+    this.salvarNoticia = (noticia, connection, callback) => {
+        connection.query('insert into noticias set ? ', noticia, callback)
+
+        //Importante lembrar que nessa forma de insert, os nomes dos campos do JSON(noticia) precisa ser os mesmos nomes dos campos
+        //que estão na entidade do banco de dados Mysql
+    }
+
     return this
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,22 @@ module.exports = app => {
 
     app.post('/noticia/salvar', (req, res) => {
         var noticia = req.body;
-        res.send(noticia)
+
+
+        var connection = app.config.dbConnection()
+        var noticiasModel = app.app.models.noticiasModel
+
+        noticiasModel.salvarNoticia(noticia, connection, (error, result) =>{
+
+
+            // Função que vai redirecionar o usuario para algum lugar, nesse caso /noticias
+            res.redirect('/noticias')
+
+            // res.render('noticias/noticias', {noticias: result});
+            // Se for renderizado do jeito acima, geraremos 2 erros:
+            // Primeiro erro -> Não terá nenhum retorno de dados em noticias
+            // Segundo erro -> Se der reload na pagina, ele vai pedir pra enviar
+            // o mesmo JSON de novo para o banco de dados, gerando outro item
+        })
     });
 }
