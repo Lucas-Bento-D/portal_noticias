67dd5111b3754515b288aaa1b2c5414cdaf632de
Lucas-Bento-D <lucasbentoskl@outlook.com>
2021-12-04 14:18:21 -0300

    Adiciona a biblioteca express-validator no projeto

--- config/server.js
+++ config/server.js
@@ -10,13 +10,20 @@ var consign = require('consign');
 var bodyParser = require('body-parser');
 
 
+// biblioteca usada para realizar validação dos dados de entrada na API
+// OBS.: Versão 6 do express-validator está quebrada atualmente, não atualize
+var expressValidator = require('express-validator');
+
+
 var app = express();
+
 
 // EJS: engine de vizualização, transporta dados do backend para o frontend
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator())
 
 consign()
     .include('app/routes')
